6d923acfb08bde37203ce632a5ea20afaf1b392d
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-04 17:49:40 +0300

    Add author user during initialization

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
 npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+
+npm run env run tests-cli "wp user create author wordpress-author@example.com --user_pass=password --role=author"
