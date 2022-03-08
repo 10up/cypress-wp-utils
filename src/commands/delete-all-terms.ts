15a7664c622a62280ebbcdec97bcc7a9f7cb9a58
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-08 17:58:25 +0300

    createTerm and deleteAllTerms commands

---
+++ src/commands/delete-all-terms.ts
@@ -0,0 +1,28 @@
+/**
+ * Delete All Terms of a given taxonomy
+ *
+ * @param taxonomy - Taxonomy to empty
+ *
+ * @example
+ * Delete all categories (note that Uncategorized term is protected)
+ * ```
+ * cy.deleteAllTerms()
+ * ```
+ *
+ * @example
+ * Delete all tags
+ * ```
+ * cy.deleteAllTerms('post_tag')
+ * ```
+ */
+export const deleteAllTerms = (taxonomy = 'category'): void => {
+  cy.visit(`/wp-admin/edit-tags.php?taxonomy=${taxonomy}`);
+  cy.get('#cb-select-all-1').click();
+
+  cy.get('body').then($body => {
+    if ($body.find('#bulk-action-selector-top').length !== 0) {
+      cy.get('#bulk-action-selector-top').select('delete');
+      cy.get('#doaction').click();
+    }
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@
 /// <reference types="cypress" />
 
 // Import commands.
+import { deleteAllTerms } from './commands/delete-all-terms';
+import { createTerm } from './commands/create-term';
 import { logout } from './commands/logout';
 import { login } from './commands/login';
 
 declare global {
   namespace Cypress {
     interface Chainable<Subject> {
+      deleteAllTerms: typeof deleteAllTerms;
+      createTerm: typeof createTerm;
       logout: typeof logout;
       login: typeof login;
     }
@@ -15,5 +19,7 @@ declare global {
 }
 
 // Register commands
+Cypress.Commands.add('deleteAllTerms', deleteAllTerms);
+Cypress.Commands.add('createTerm', createTerm);
 Cypress.Commands.add('logout', logout);
 Cypress.Commands.add('login', login);
