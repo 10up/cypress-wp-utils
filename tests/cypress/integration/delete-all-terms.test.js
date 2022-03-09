1b43a7903588288d7897a8d2d90e62c1cdb0c6af
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-09 17:51:22 +0300

    Tests for paginated terms deletion

--- tests/cypress/integration/delete-all-terms.test.js
+++ tests/cypress/integration/delete-all-terms.test.js
@@ -4,11 +4,40 @@ describe('Command: deleteAllTerms', () => {
     cy.login();
   });
 
+  after(() => {
+    // Restore default 20 items per page
+    cy.visit(`/wp-admin/edit-tags.php?taxonomy=category`);
+    cy.get('#show-settings-link').click();
+    cy.get('input.screen-per-page').click().clear().type(20);
+    cy.get('#screen-options-apply').click();
+
+    cy.visit(`/wp-admin/edit-tags.php?taxonomy=post_tag`);
+    cy.get('#show-settings-link').click();
+    cy.get('input.screen-per-page').click().clear().type(20);
+    cy.get('#screen-options-apply').click();
+  });
+
   it('Should be able to Delete All Categories', () => {
     cy.createTerm('Term to delete');
     cy.createTerm('Term to delete 2');
     cy.deleteAllTerms();
     cy.get('.notice').should('contain', 'Categories deleted');
+    cy.get('#the-list a.row-title').should('have.length', 1);
+    cy.get('#the-list a.row-title')
+      .first()
+      .should('have.text', 'Uncategorized');
+  });
+
+  it('Should be able to delete paginated categories', () => {
+    for (let index = 0; index < 10; index++) {
+      cy.createTerm('Multi page category ' + index);
+    }
+    // Set 2 items per page
+    cy.get('#show-settings-link').click();
+    cy.get('input.screen-per-page').click().clear().type(2);
+    cy.get('#screen-options-apply').click();
+
+    cy.deleteAllTerms();
     cy.get('#the-list a.row-title').should('have.length', 1);
     cy.get('#the-list a.row-title')
       .first()
@@ -21,5 +50,21 @@ describe('Command: deleteAllTerms', () => {
     cy.deleteAllTerms('post_tag');
     cy.get('.notice').should('contain', 'Tags deleted');
     cy.get('#the-list a.row-title').should('have.length', 0);
+  });
+
+  it('Should be able to delete paginated tags', () => {
+    for (let index = 0; index < 10; index++) {
+      cy.createTerm('Multi page tag ' + index, 'post_tag');
+    }
+    // Set 2 items per page
+    cy.get('#show-settings-link').click();
+    cy.get('input.screen-per-page').click().clear().type(2);
+    cy.get('#screen-options-apply').click();
+
+    cy.deleteAllTerms();
+    cy.get('#the-list a.row-title').should('have.length', 1);
+    cy.get('#the-list a.row-title')
+      .first()
+      .should('have.text', 'Uncategorized');
   });
 });
