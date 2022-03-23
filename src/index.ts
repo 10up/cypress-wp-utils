fc5b62c1e8bc43f41c0961217726d38367268b47
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-23 12:37:48 +0300

    Added deactivateAllPlugins command

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 /// <reference types="cypress" />
 
 // Import commands.
+import { deactivateAllPlugins } from './commands/deactivate-all-plugins';
 import { activatePlugin } from './commands/activate-plugin';
 import { getCurrentPlugin } from './helpers/get-current-plugin';
 import { deleteAllTerms } from './commands/delete-all-terms';
@@ -12,6 +13,7 @@ import { login } from './commands/login';
 declare global {
   namespace Cypress {
     interface Chainable<Subject> {
+      deactivateAllPlugins: typeof deactivateAllPlugins;
       activatePlugin: typeof activatePlugin;
       getCurrentPlugin: typeof getCurrentPlugin;
       deleteAllTerms: typeof deleteAllTerms;
@@ -23,6 +25,7 @@ declare global {
 }
 
 // Register commands
+Cypress.Commands.add('deactivateAllPlugins', deactivateAllPlugins);
 Cypress.Commands.add('activatePlugin', activatePlugin);
 Cypress.Commands.add('getCurrentPlugin', getCurrentPlugin);
 Cypress.Commands.add('deleteAllTerms', deleteAllTerms);
