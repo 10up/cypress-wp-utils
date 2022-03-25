efec7a619a85253024398bfda27636a63e884ea7
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-25 10:24:22 +0300

    Documentation for cli commands

--- src/commands/wp-cli-eval.ts
+++ src/commands/wp-cli-eval.ts
@@ -1,9 +1,14 @@
 /**
- * Wp Cli Eval
+ * Run PHP code as WP CLI eval-file command
+ *
+ * @param command - PHP code to execute with WP CLI. The '<?php ' prefix is optional.
  *
  * @example
  * ```
- * cy.wpCliEval()
+ * cy.wpCliEval('<?php for($i=0; $i<10; $i++) { echo $i; }').then(response=>{
+ *   const output = response.stdout;
+ *   // Do whatever with the output.
+ * })
  * ```
  */
 export const wpCliEval = (command: string): void => {
--- src/commands/wp-cli.ts
+++ src/commands/wp-cli.ts
@@ -1,9 +1,15 @@
 /**
- * Wp Cli
+ * Perform a WP CLI command
+ *
+ * @param command - WP CLI command. The 'wp ' prefix is optional
+ * @param ignoreFailures - Prevent command to fail if CLI command exits with error
  *
  * @example
  * ```
- * cy.wpCli()
+ * cy.wpCli('plugin list --field=name').then(response=>{
+ *   const plugins = res.stdout.split('\n');
+ *   // Do whatever with plugins list
+ * });
  * ```
  */
 export const wpCli = (command: string, ignoreFailures = false): void => {
