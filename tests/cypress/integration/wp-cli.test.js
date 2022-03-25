f594a22460b062543477919f9ae822a25b0dc7f7
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-03-25 09:49:10 +0300

    Separate tests for wp cli

--- tests/cypress/integration/wp-cli.test.js
+++ tests/cypress/integration/wp-cli.test.js
@@ -1,21 +1,18 @@
 describe('Command: wpCli', () => {
-  it('Perform tests of cli commands', () => {
-    cy.visit('/');
-
+  it('Should run cli command and receive the response', () => {
     cy.wpCli('cli version')
       .its('stdout')
       .should('match', /^WP-CLI \d+\.\d+/);
+  });
 
-    cy.wpCli('plugin list --field=name')
-      .its('stdout')
-      .should('contain', 'cypress-wp-utils');
-
-    // Should not fail of the cli command exit with error
+  it('Should not fail with ignoreFailures=true', () => {
     const randomCommand =
       'command' + (Math.random() + 1).toString(16).substring(3);
     cy.wpCli(randomCommand, true).its('code').should('equal', 1);
+  });
 
-    const evalSting = (Math.random() + 1).toString(16);
+  it('Should run cli in eval mode', () => {
+    const evalSting = (Math.random() + 1).toString(16).substring(2);
     cy.wpCliEval(`<?php echo 'Test Eval Command ${evalSting}';`)
       .its('stdout')
       .should('contain', evalSting);
