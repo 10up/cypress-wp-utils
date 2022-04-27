c87f93a25d62cc7f650a46aa8f8f242e8d466490
Faisal Alvi <faisal.alvi@get10up.com>
2022-04-27 17:55:35 +0530

    add examples in the documentaion

--- src/commands/check-block-pattern-exists.ts
+++ src/commands/check-block-pattern-exists.ts
@@ -1,9 +1,26 @@
 /**
  * Check Block Pattern Exists. Works only with WordPress >=5.5
  *
+ * @param postData {
+ *          `title` - Patttern name/title,
+ *          `categoryValue` - Value of the pattern category,
+ *        }
+ *
  * @example
+ * For WP v5.5
  * ```
- * cy.checkBlockPatternExists()
+ * cy.checkBlockPatternExists({
+ *    title: 'Two buttons',
+ * });
+ * ```
+ *
+ * @example
+ * For WP v5.9
+ * ```
+ * cy.checkBlockPatternExists({
+ *    title: 'Three columns with offset images',
+ *    categoryValue: 'gallery',
+ * });
  * ```
  */
 export const checkBlockPatternExists = ({
--- tests/cypress/integration/check-block-pattern-exists.test.js
+++ tests/cypress/integration/check-block-pattern-exists.test.js
@@ -4,14 +4,6 @@ describe('Command: checkBlockPatternExists', () => {
     cy.deactivatePlugin('classic-editor');
   });
 
-  // For WP v5.5
-  it('Should be able to Check Block Pattern Exists', () => {
-    cy.checkBlockPatternExists({
-      title: 'Two buttons',
-    });
-  });
-
-  // For WP v5.9
   it('Should be able to Check Block Pattern Exists', () => {
     cy.checkBlockPatternExists({
       title: 'Three columns with offset images',
