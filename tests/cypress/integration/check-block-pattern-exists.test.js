f2f4ecdaa911d6b33e7526ae680593f3769049e3
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-06-03 14:40:49 +0300

    Make test optional for old WordPress core

--- tests/cypress/integration/check-block-pattern-exists.test.js
+++ tests/cypress/integration/check-block-pattern-exists.test.js
@@ -1,29 +1,36 @@
 const { randomName } = require('../support/functions');
-
-const testPatterns = [
-  { title: 'Quote', cat: 'text', expected: true },
-  { title: randomName(), cat: 'text', expected: false },
-  { title: 'Quote', cat: randomName(), expected: false },
-];
+const semver = require('semver');
 
 describe('Command: checkBlockPatternExists', () => {
-  before(() => {
-    cy.login();
-    cy.deactivatePlugin('classic-editor');
-  });
+  if (semver.gte(Cypress.env('WORDPRESS_CORE').toString(), '5.5.0')) {
+    before(() => {
+      cy.login();
+      cy.deactivatePlugin('classic-editor');
+    });
 
-  testPatterns.forEach(testCase => {
-    const shouldIt = testCase.expected ? 'should' : 'shoult not';
-    it(`Pattern "${testCase.title}" ${shouldIt} exist in category "${testCase.cat}"`, () => {
-      cy.checkBlockPatternExists({
-        title: testCase.title,
-        categoryValue: testCase.cat,
-      }).then(exists => {
-        assert(
-          exists === testCase.expected,
-          `Pattern "${testCase.title}" in category "${testCase.cat}": ${testCase.expected}`
-        );
+    const testPatterns = [
+      { title: 'Quote', cat: 'text', expected: true },
+      { title: randomName(), cat: 'text', expected: false },
+      { title: 'Quote', cat: randomName(), expected: false },
+    ];
+
+    testPatterns.forEach(testCase => {
+      const shouldIt = testCase.expected ? 'should' : 'shoult not';
+      it(`Pattern "${testCase.title}" ${shouldIt} exist in category "${testCase.cat}"`, () => {
+        cy.checkBlockPatternExists({
+          title: testCase.title,
+          categoryValue: testCase.cat,
+        }).then(exists => {
+          assert(
+            exists === testCase.expected,
+            `Pattern "${testCase.title}" in category "${testCase.cat}": ${testCase.expected}`
+          );
+        });
       });
     });
-  });
+  } else {
+    it('Skip Block Pattern test, WordPress version too low', () => {
+      assert(true);
+    });
+  }
 });
