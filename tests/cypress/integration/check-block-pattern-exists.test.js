c5615d8ad5cc1a703a1b14db1f6e256c422ace00
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-06-03 14:01:06 +0300

    Tests for checkBlockPatterExists

--- tests/cypress/integration/check-block-pattern-exists.test.js
+++ tests/cypress/integration/check-block-pattern-exists.test.js
@@ -1,19 +1,30 @@
+const { randomName } = require('../support/functions');
+
+const testPatterns = [
+  { title: 'Quote', cat: 'text', expected: true },
+  { title: randomName(), cat: 'text', expected: false },
+  { title: 'Quote', cat: randomName(), expected: false },
+];
+
 describe('Command: checkBlockPatternExists', () => {
   before(() => {
     cy.login();
     cy.deactivatePlugin('classic-editor');
   });
 
-  it('Should be able to Check Block Pattern Exists', () => {
-    cy.checkBlockPatternExists({
-      title: 'Quote',
-      categoryValue: 'text',
-    }).then(exists => {
-      if (exists) {
-        // The block patter exists!
-      } else {
-        // The block pattern does not exist!
-      }
+  testPatterns.forEach(testCase => {
+    const shouldIt = testCase.expected ? 'should' : 'shoult not';
+    it(`Pattern "${testCase.title}" ${shouldIt} exist in category "${testCase.cat}"`, () => {
+      console.log(Cypress.config('wpEnv'));
+      cy.checkBlockPatternExists({
+        title: 'Quote',
+        categoryValue: 'text',
+      }).then(exists => {
+        assert(
+          exists === testCase.expected,
+          `Pattern "${testCase.title}" in category "${testCase.cat}": ${testCase.expected}`
+        );
+      });
     });
   });
 });
