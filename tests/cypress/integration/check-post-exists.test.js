2e1f2d08e7a77936090e40131b274453eb6d91eb
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-06-13 22:14:04 +0300

    Ignore WP 5.2 Synchronous XHR error

--- tests/cypress/integration/check-post-exists.test.js
+++ tests/cypress/integration/check-post-exists.test.js
@@ -28,6 +28,17 @@ describe('Command: checkPostExists', () => {
     cy.login();
     cy.deactivatePlugin('classic-editor');
 
+    // Ignore WP 5.2 Synchronous XHR error.
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      if (
+        err.message.includes(
+          "Failed to execute 'send' on 'XMLHttpRequest': Failed to load 'http://localhost:8889/wp-admin/admin-ajax.php': Synchronous XHR in page dismissal"
+        )
+      ) {
+        return false;
+      }
+    });
+
     // Create posts which expected to exist during tests
     tests.forEach(test => {
       if (test.expected) {
