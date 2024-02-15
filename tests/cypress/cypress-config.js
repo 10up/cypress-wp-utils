c594550853766f1f8da81f1da69ab0d413d1633e
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-02-16 10:25:36 +1100

    Remove retries following switch to Chrome.

--- tests/cypress/cypress-config.js
+++ tests/cypress/cypress-config.js
@@ -8,7 +8,6 @@ module.exports = defineConfig({
   videosFolder: 'tests/cypress/videos',
   downloadsFolder: 'tests/cypress/downloads',
   video: false,
-  retries: 2,
   e2e: {
     setupNodeEvents(on, config) {
       return setBaseUrl(on, config);
